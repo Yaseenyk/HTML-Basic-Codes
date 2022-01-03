66fbbd2c3617d1ffdb0ac521c8d36e74816e0fec
Yaseen YK <45963355+Yaseenyk@users.noreply.github.com>
2022-01-03 20:28:41 +0530

    Basic HTML Code.

---
+++ new.js
@@ -0,0 +1,27 @@
+// const arr=(2,2,3,4,2);
+// const arr2=[2,3,"Hello",4,2];
+
+// arr.push(5);
+// arr.pop(5);
+// arr.length;
+
+
+//objects Silmilar to hasmaps in java.
+
+const a = {};
+
+const Bumbulbee={
+    "name":"Something",
+    "Enrollment No":"",
+    "contact" : [726364723,273464757],
+    "parentDetails":{
+        name:"YK",
+    },
+    "Foo ": function(){
+        console.log("Hello");
+    },
+};
+console.log(Bumbulbee.name);
+console.log(Bumbulbee["contact"]);
+console.log("name" in Bumbulbee);
+console.log("names Another" a.name); 
---
+++ newjss.js
@@ -0,0 +1,7 @@
+function showFunctionality(){
+    alert("This is a message");
+    const userInput=prompt("Take some Input");
+    console.log("User Input is",userInput);
+    const userInput = confirm("Are u Sure");
+    console.log("User Input is",userInput);
+}
